2ee77b12ca32627eef2763ad5757639b2172986c
ben ben <1349769271@qq.com>
2023-12-01 20:36:35 +0800

    完成合约和测试内容

--- test/market.js
+++ test/market.js
@@ -1,9 +1,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { eth } = require("ethers");
 
 describe("Market", function () {
   let usdt, market, myNft, account1, account2;
-  let baseURI = "https://sameple.com/";
+  let baseURI = "http://127.0.0.1:8545/";
 
   beforeEach(async () => {
     [account1, account2] = await ethers.getSigners();
@@ -11,7 +12,7 @@ describe("Market", function () {
     const USDT = await ethers.getContractFactory("cUSDT");
     usdt = await USDT.deploy();
     const MyNFT = await ethers.getContractFactory("MyNFT");
-    myNft = await MyNFT.deploy();
+    myNft = await MyNFT.deploy(account1.address);
     const Market = await ethers.getContractFactory("Market");
     market = await Market.deploy(usdt.target, myNft.target);
     // console.log(account1)
@@ -44,22 +45,59 @@ it('account2 should have 0 nfts', async function() {
   expect(await myNft.balanceOf(account2.address)).to.equal(0);
 });
 
-it('account1 can list two nfts to market', async function() {
-  const price = "0x0000000000000000000000000000000000000000000000000001c6bf52634000";
-  // let price = "0x0100"
+  it('account1 can list two nfts to market', async function() {
+    const price = "0x0000000000000000000000000000000000000000000000000001c6bf52634000";
+    await market.listNFT(0, price);
+    await market.listNFT(0, price);
 
-})
+    const listedNFTs = await market.getListedNFTs();
+    expect(listedNFTs).to.have.lengthOf(2);
+    expect(listedNFTs[0].owner).to.equal(account1.address);
+    expect(listedNFTs[1].owner).to.equal(account1.address);
+  });
 
-it('account1 can unlist one nft from market', async function() {
-  const price = "0x0000000000000000000000000000000000000000000000000001c6bf52634000";
-  
-})
+  it('account1 can unlist one nft from market', async function() {
+    const price = "0x0000000000000000000000000000000000000000000000000001c6bf52634000";
+    await market.listNFT(0, price);
+    await market.listNFT(1, price);
 
-it('account1 can change price of nft from market', async function() {
-  
-})
+    await market.unlistNFT(0);
 
-it('account2 can buy nft from market', async function() {
-  
-})
+    const listedNFTs = await market.getListedNFTs();
+    expect(listedNFTs).to.have.lengthOf(1);
+    expect(listedNFTs[0].owner).to.equal(account1.address);
+  });
+
+  it('account1 can change price of nft from market', async function() {
+    const price = "0x0000000000000000000000000000000000000000000000000001c6bf52634000";
+    const newPrice = "0x0000000000000000000000000000000000000000000000000002c6bf52634000";
+    await market.listNFT(0, price);
+
+    await market.changeNFTPrice(0, newPrice);
+
+    const listedNFTs = await market.getListedNFTs();
+    expect(listedNFTs).to.have.lengthOf(1);
+    expect(listedNFTs[0].owner).to.equal(account1.address);
+
+    const listedNFTPrice = await market.getNFTPrice(0);
+    expect(listedNFTPrice).to.equal(newPrice);
+  });
+
+  it('account2 can buy nft from market', async function() {
+    const price = "0x20";
+    await market.listNFT(0, price);
+
+    const balanceBefore = await usdt.balanceOf(account2.address);
+    const ownerBefore = await myNft.ownerOf(0);
+
+    await market.connect(account2).buyNFT(0,{
+      value:"0x20"
+    });
+
+    const balanceAfter = await usdt.balanceOf(account2.address);
+    const ownerAfter = await myNft.ownerOf(0);
+    // 10000000000000000000000
+
+    expect(ownerAfter).to.equal(account2.address);
+  });
 })
